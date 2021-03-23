819ba231fa3c5cf0b278f032a06d210ca6417f04
Urmas-Villem Sundja <urmas-villem@users.noreply.github.com>
2021-03-23 15:39:51 +0200

    Esimese tunni esimene pool(esimesed php sammud)

---
+++ page.php
@@ -0,0 +1,29 @@
+<?php
+	$myname = "Urmas-Villem Sundja";
+	$currenttime = date("d.m.Y H:i:s");
+	$timehtml = "\n <p> Lehe avamise hetkel oli kuupäev ja kellaaeg: " .$currenttime ."</p>\n";
+	$semesterbegin = new DateTime("2021-1-25");
+	$semesterend = new DateTime("2021-6-30");
+	$semesterduration = $semesterbegin->diff($semesterend);
+	$semesterdurationdays = $semesterduration->format("%r%a");
+	$semesterdurhtml = "\n <p>2021 kevadsemester kestus on " .$semesterdurationdays ." päeva.</p> \n";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title>Veebirakendused ja nende loomine 2021</title>
+</head>
+<body>
+	<h1>
+		<?php 
+			echo $myname;
+		?>
+	</h1>
+	<p>See leht on valminud õppetöö raames!</p>
+		<?php
+			echo $timehtml;
+			echo $semesterdurhtml;
+		?>
+</body>
+</html>
